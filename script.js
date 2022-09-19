729d8a0d396ae780b806568a2aa4eb802ececb0f
tashisrael <tashisrael04@gmail.com>
2022-09-19 18:58:50 +0300

    Correct linter errors

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const addBook = document.querySelector('#btn');
 const bookList = document.querySelector('#list');
 const form = document.querySelector('#form');
-
 const Book = function objBook(title, author) {
-	this.title = title;
-	this.author = author;
+this.title = title;
+this.author = author;
 };
-
-let storedBooks = [];
-
+const storedBooks = [];
 function addBooks(newBook) {
 const bookStore = `<div class = "book">
 <h2> Title: ${newBook.title}</h2> 
@@ -19,8 +16,6 @@ const bookStore = `<div class = "book">
 bookList.innerHTML += bookStore;
 return bookList.innerHTML;
 }
-
-//local storage
 let localForm = { title: '', author: ''};
 if (localStorage.localForm) {
 localForm = JSON.parse(localStorage.localForm);
@@ -32,12 +27,11 @@ localStorage.localForm = JSON.stringify(localForm);
 localForm.title = title.value;
 localForm.author = author.value;
 });
-
 addBook.addEventListener('click', (e) => {
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 if (title.value === '' || author.value === '') {
-	e.preventDefault();
+e.preventDefault();
 }
 else{
 const newBook = new Book(title.value, author.value);
@@ -45,8 +39,6 @@ addBooks(newBook);
 title.value ='';
 author.value = '';}
 });
-
-//Remove books
 bookList.addEventListener('click', (eve) => {
 if(eve.target.classList.contains('delete')) {
 document.querySelector('.list').removeChild(eve.target.parentElement);
